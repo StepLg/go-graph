a0feb3ae401a4a253d0bc434bdf0cfacb44551c1
Stephan Kountso aka StepLg <steplg@gmail.com>
2010-04-22 16:36:25 +0400

    add testing with gospec

--- src/graph/MatrixUndirected_test.go
+++ src/graph/MatrixUndirected_test.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"erx"
 	"testing"
+	"github.com/orfjackal/gospec/src/gospec"
 )
 
 func CheckErx(t *testing.T, err erx.Error) {
@@ -12,13 +13,34 @@ func CheckErx(t *testing.T, err erx.Error) {
 	}
 }
 
-func CheckEqBool(t *testing.T, l, r bool, msg string) {
+func CheckEq(t *testing.T, l, r interface{}, msg string) {
 	if (l!=r) {
 		t.Error(msg)
 	}
 }
 
-func Test_Edges(t *testing.T) {
+func NodesBinarySearch(nodes Nodes, n NodeId) bool {
+	if len(nodes)==0 {
+		return false
+	}
+	
+	switch len(nodes) {
+		case 0 : return false
+		case 1 : return nodes[0]==n
+		default:
+			m := len(nodes)>>1
+			if nodes[m]==n {
+				return true
+			} else if nodes[m]<n {
+				return NodesBinarySearch(nodes[0:m], n)
+			} else {
+				return NodesBinarySearch(nodes[m+1:], n)
+			}
+	}
+	return false
+}
+
+func Test_MatrixUndirected_Edges(t *testing.T) {
 	gr := NewUndirectedGraphMatrix(5)
 	var err erx.Error
 	_, err = gr.AddEdge(1, 2)
@@ -33,21 +55,40 @@ func Test_Edges(t *testing.T) {
 	var conn bool
 	conn, err = gr.CheckEdgeBetween(1, 2)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, true, "Missing edge 1--2")
+	CheckEq(t, conn, true, "Missing edge 1--2")
 	conn, err = gr.CheckEdgeBetween(3, 2)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, true, "Missing edge 3--2")
+	CheckEq(t, conn, true, "Missing edge 3--2")
 	conn, err = gr.CheckEdgeBetween(4, 2)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, true, "Missing edge 4--2")
+	CheckEq(t, conn, true, "Missing edge 4--2")
 	conn, err = gr.CheckEdgeBetween(1, 4)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, true, "Missing edge 1--4")
+	CheckEq(t, conn, true, "Missing edge 1--4")
 
 	conn, err = gr.CheckEdgeBetween(1, 3)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, false, "Phantom edge 1--3")
+	CheckEq(t, conn, false, "Phantom edge 1--3")
 	conn, err = gr.CheckEdgeBetween(4, 3)
 	CheckErx(t, err)
-	CheckEqBool(t, conn, false, "Phantom edge 4--3")
+	CheckEq(t, conn, false, "Phantom edge 4--3")
+}
+
+func MatrixUndirectedInternalSpec(c gospec.Context) {
+	gr := NewUndirectedGraphMatrix(5)
+	
+	c.Specify("Correct edge ids generation to first node", func() {
+		for i := 2; i < gr.GetCapacity(); i++ {
+			connId, err := gr.getConnectionId(1, NodeId(i), true)
+			c.Expect(err, gospec.IsNil)
+			c.Expect(int(connId), gospec.Equals, i-2)
+			c.Expect(gr.GetSize(), gospec.Equals, i)
+		}
+	})
+}
+
+func TestMatrixUndirectedSpec(t *testing.T) {
+	r := gospec.NewRunner()
+	r.AddSpec("MatrixUndirectedInternalSpec", MatrixUndirectedInternalSpec)
+	gospec.MainGoTest(r, t)
 }
